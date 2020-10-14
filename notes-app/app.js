076b7976f5696d449ddc92e4cddc19a237e621a0
Michael Hofweller <michael.hofweller@gmail.com>
2020-10-14 15:19:13 -0700

    new course start

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,18 @@
-const fs = require('fs');
+const chalk = require('chalk');
+const getNotes = require('./notes.js');
 
-fs.writeFileSync('notes.txt', 'this file was created by node.js');
+const notes = getNotes();
+
+console.log(chalk.bold.inverse.green(notes));
+
+// const validator = require('validator');
+// const getNotes = require('./notes.js');
+
+// const notes = getNotes();
+
+// console.log(validator.isEmail('michael.hofweller@gmail.com'));
+// console.log(validator.isURL('www.google.com'));
+
+// const add = require('./utils.js');
+
+// console.log(add(2, 1));
---
+++ notes-app/notes.js
@@ -0,0 +1,5 @@
+function getNotes() {
+        return 'SUCCESS!';
+}
+
+module.exports = getNotes;
