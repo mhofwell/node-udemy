5730b6320f4eddfd8600c90e97a94b4a2e055954
Michael Hofweller <michael.hofweller@gmail.com>
2020-11-07 16:08:09 -0800

    new

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,6 +1,10 @@
-console.log('Starting');
+const request = require('postman-request');
 
-setTimeout(function() {
-        console.log('2s Timer');
-}, 2000);
-console.log('Stopping');
+const url = 'http://api.weatherstack.com/current?access_key=51db1d692b4c5f27f2f7d0ace18616b3&query=37.8267,-122.4233';
+
+function httpResponse(error, response) {
+        const data = JSON.parse(response.body);
+        console.log(data.current);
+}
+
+request({ url }, httpResponse);
